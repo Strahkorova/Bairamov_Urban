2765d7f00906761257b401d17492323706b25bec
Jo <bairamov.dzhamil@mail.ru>
2024-01-17 21:43:16 +0400

    Организация программ и методы строк

--- Module_1/Listen_organization_programm.py
+++ Module_1/Listen_organization_programm.py
@@ -1,1 +1,8 @@
-print('Heello world')
+my_string = "Телеграфия"
+string = "Что-то интересное наверное"
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
