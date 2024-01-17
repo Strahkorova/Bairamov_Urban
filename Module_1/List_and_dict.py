f5c171a5a1610d2866ee4d02b63b774aa4fbb2bb
Jo <bairamov.dzhamil@mail.ru>
2024-01-17 23:57:25 +0400

    Work with dictionary

---
+++ Module_1/List_and_dict.py
@@ -0,0 +1,17 @@
+# Работа со списком
+my_list = ['red', 'blue', 'black', 'white', 'orange', 'green']
+print('My list = ', my_list)
+
+print('First element list = ', my_list[0])
+print('Last element list = ', my_list[-1])
+print('My sublist = ', my_list[2:5])
+my_list[2] = 'yellow'
+print('My new list = ', my_list)
+
+# Работа со словарем
+my_dict = {'red': 'красный', 'blue': 'голубой', 'black': 'черный', 'white': 'белый'}
+print('My dictionary = ', my_dict)
+print('Value my dictionare with key = ', my_dict['black'])
+my_dict['blue'] = 'синий'
+my_dict['green'] = 'зеленый'
+print('My new dictionary = ', my_dict)
