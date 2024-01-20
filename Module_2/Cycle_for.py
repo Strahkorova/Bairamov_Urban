6f9036edd7dc140d023686409176b0a20d77efc8
Jo <bairamov.dzhamil@mail.ru>
2024-01-20 12:02:21 +0400

    Module_2 - Cycle_for

---
+++ Module_2/Cycle_for.py
@@ -0,0 +1,8 @@
+my_car = ['BMW', 'Mercedes', 'LADA', 'OMODA', 'Ferrari']
+my_text = 'Я езжу на автомобиле марки - '
+cars_count = 0
+for x in my_car:
+    print(my_text + x)
+    print(cars_count)
+    cars_count += 10
+
