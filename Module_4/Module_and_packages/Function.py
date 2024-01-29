8b5c42cf69a5429953562a97b37e0303ca4f14fa
Jo <bairamov.dzhamil@mail.ru>
2024-01-29 20:00:37 +0400

    Module_4 - module_and_package

---
+++ Module_4/Module_and_packages/Function.py
@@ -0,0 +1,6 @@
+def function_1():
+    print("Hello my friend")
+
+
+def function_2(name):
+    print("My name is " + name)
---
+++ Module_4/Module_and_packages/Print_name.py
@@ -0,0 +1,9 @@
+import Function as fun
+
+import Module_4.Module_and_packages.Function as Fun
+
+fun.function_1()
+fun.function_2("Джамиль")
+
+Fun.function_1()
+Fun.function_2("Максим")
