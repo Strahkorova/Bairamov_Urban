98e418472b0de88401bbee9daaf30d11cff053d0
Jo <bairamov.dzhamil@mail.ru>
2024-02-21 23:26:46 +0400

    Module_5 - special_method v2

--- Module_5/special_method.py
+++ Module_5/special_method.py
@@ -1,12 +1,19 @@
 
 class House:
-
     def __init__(self):
         self.number = 0
 
     def start(self, i):
         self.number = i
-        print("Номер = ", i)
+        print("Номер = ", self.number)
 
 
+class House1:
+    number = 0
+
+    def start(self, i):
+        self.number = i
+        print("Номер1 = ", self.number)
+
+House1().start(5)
 House().start(8)
