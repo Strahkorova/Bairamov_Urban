b7091c146d089761ea0fb6b9da56d5bf48da5fa3
Jo <bairamov.dzhamil@mail.ru>
2024-03-10 00:13:57 +0400

    Module_6 - nasledovanie_class.py

---
+++ Module_6/nasledovanie_class.py
@@ -0,0 +1,26 @@
+class Car:
+    price = 1000000
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return '{} Марка {}'.format(self.__class__.__name__, self.name)
+
+    def horse_powers(self, power):
+        print("Количество лошадинных сил = " + str(power) + ". Стомость машины = " + str(self.price))
+
+class Nissan(Car):
+    price = 50000
+
+    def horse_powers(self, power):
+        print("Количество лошадинных сил " + self.name + " = " + str(power) + ". Стомость машины = " + str(self.price))
+
+class Kia(Car):
+    price = 80000
+
+    def horse_powers(self, power):
+        print("Kia " + self.name + " имеет " + str(power) + " лощадиных сил. Стомость машины = " + str(self.price))
+
+car = Kia("Sorento")
+print(car)
+print(car.horse_powers(230))
