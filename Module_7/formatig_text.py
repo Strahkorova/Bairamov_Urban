fecb8d4222619d9919a5312b7c596cfdbc0d9efa
Jo <bairamov.dzhamil@mail.ru>
2024-03-14 20:56:25 +0400

    Module_7 - formating_text.py v4

--- Module_7/formatig_text.py
+++ Module_7/formatig_text.py
@@ -3,19 +3,20 @@ class team():
     dict_team = {}
     dict_score = {}
     dict_time = {}
+    list_time_arg = []
 
     def team_name(self):
         name = input("Введите название команды - ")
         team_num = input("Введите количество участников - ")
-        # if 4 < int(team_num) < 21:
-        #     print('В команде %s, %s участников' % (name, team_num))
-        # else:
-        #     if team_num[-1] == '1':
-        #         print('В команде %s, %s участник' % (name, team_num))
-        #     elif team_num[-1] == '2' or team_num[-1] == '3' or team_num[-1] == '4':
-        #         print('В команде %s, %s участника' % (name, team_num))
-        #     else:
-        #         print('В команде %s, %s участников' % (name, team_num))
+        if 4 < int(team_num) < 21:
+            print('В команде %s, %s участников' % (name, team_num))
+        else:
+            if team_num[-1] == '1':
+                print('В команде %s, %s участник' % (name, team_num))
+            elif team_num[-1] == '2' or team_num[-1] == '3' or team_num[-1] == '4':
+                print('В команде %s, %s участника' % (name, team_num))
+            else:
+                print('В команде %s, %s участников' % (name, team_num))
         team.list_team_name.append(name)
         team.dict_team[name] = team_num
 
@@ -32,37 +33,46 @@ class team():
         for i in list:
             score = input('Введите количество решенных задач командой {0} - '.format(i))
             time = input('Введите время за которое команда {0} решила задачи - '.format(i))
-            team.dict_score[i] = score
-            team.dict_time[i] = time
+            self.dict_score[i] = score
+            self.dict_time[i] = time
+            self.list_time_arg.append(float(time)/float(score))
+
+class rules():
+    file_name = 'Правила.txt'
+    def play_rules(self):
+        with open(self.file_name, mode='r', encoding='utf8') as file:
+            for line in file:
+                print(line)
+        print(file.closed)
 
 
-class play(team):
 
-    list_1 = team.list_team_name
-    dict_sc = team.dict_score
-    dict_t = team.dict_time
-    def result(self):
-        score_1 = float(self.dict_sc[self.list_1[0]])
-        score_2 = float(self.dict_sc[self.list_1[1]])
-        time_1 = float(play.dict_time[self.list_1[0]])
-        time_2 = float(play.dict_time[self.list_1[1]])
-        if score_1 > score_2:
-            print('Команда - {0} победила!'.format(self.list_1[0]))
-        elif score_1 < score_2:
-            print('Команда - {0} победила!'.format(self.list_1[1]))
+class play(team, rules):
+
+    def challenge_result(self):
+        time_arg_1 = float(self.list_time_arg[0])
+        time_arg_2 = float(self.list_time_arg[1])
+        score_1 = float(self.dict_score[self.list_team_name[0]])
+        score_2 = float(self.dict_score[self.list_team_name[1]])
+
+        if time_arg_1 > time_arg_2 or time_arg_1 == time_arg_2 and score_1 > score_2:
+            print('Команда {0} победила затратив на одну задачу - {1:.3f}'.format(self.list_team_name[0], time_arg_1))
+        elif time_arg_1 < time_arg_2 or time_arg_1 == time_arg_2 and score_1 < score_2:
+            print('Команда {0} победила затратив на одну задачу - {1:.3f}'.format(self.list_team_name[0], time_arg_1))
         else:
-            time_avg_1 = score_1/time_1
-            time_avg_2 = score_2/time_2
-            if time_avg_1 > time_avg_2:
-                print('Команда {0} победила затратив на одну задачу - {1:.3f}'.format(self.list_1[0], time_avg_1))
-            else:
-                print('Команда {0} победила затратив на одну задачу - {1:.3f}'.format(self.list_1[1], time_avg_2))
-
-
+            task_total = score_1 + score_2
+            time = (time_arg_1+time_arg_2)/2
+            print('Команда {0} и {1} сиграли в ничью! Вместе они решили - {2:.2f} задачи и затратили '
+                  'на решение одной задачи в среднем - {3:2f}'.format(self.list_team_name[0], self.list_team_name[1], task_total, time))
 
 t = play()
+t.play_rules()
 t.team_name()
 t.team_name()
 t.team_score_and_time()
-t.result()
+t.challenge_result()
+t.team_all()
+
+
+
 
