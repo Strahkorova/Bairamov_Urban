391ad4c7a3069551a23e55bbcc3ef15c0b47ac3c
Jo <bairamov.dzhamil@mail.ru>
2024-04-17 22:09:41 +0400

    Module_9 - stream_bank.py

--- Module_10/stream_bank.py
+++ Module_10/stream_bank.py
@@ -3,6 +3,7 @@ import time
 from threading import Thread
 
 class banks(Thread):
+
     def __init__(self, deposit, withdrawal):
         self.deposit = deposit
         self.withdrawal = withdrawal
@@ -12,41 +13,39 @@ class banks(Thread):
 
     #пополнение счета
     def replenishment(self):
+        R = self.deposit+800
         print(f"На банксовком счету {self.deposit} руб!")
-        while self.deposit < 1500:
+        while self.deposit < R:
             with self.lock:
                 if self.deposit != 0:
-                    self.deposit = 100 + self.deposit
+                    self.deposit = self.withdrawal + self.deposit
                     print(f'Накапали проценты на дипозит. Денег на счете - {self.deposit}')
                 else:
                     print(f'Ваш счет равен 0 руб.')
                     break
                 time.sleep(0.2)
 
-    #снятие наличных
     def Withdrawal(self):
         while self.deposit != 0:
             with self.lock:
-                if self.deposit >= 150:
+                if self.deposit >= self.withdrawal:
                     self.deposit = self.deposit - self.withdrawal
                     print(f'С дипазита сняли деньги! Денег на счете - {self.deposit}')
                 else:
                     print('Снимать деньги нельзя! У вас на счете минимальный допустимый остаток')
-                    #break
+                    break
                 time.sleep(0.2)
 
 
 
-bank = banks(1000, 200)
+bank = banks(1000, 100)
 
 
 deposito = threading.Thread(target=bank.replenishment)
 withdra = threading.Thread(target=bank.Withdrawal)
 
 deposito.start()
-
-deposito.join()
 withdra.start()
 
-
+deposito.join()
 withdra.join()
